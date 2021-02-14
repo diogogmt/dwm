0e3583f298ae330dc8eba006fbdc740456ef9fb3
Diogo Monteiro <diogo@blockthrough.com>
2021-02-14 18:24:02 +0000

    updating firefox tag rule and quit and killclient shortcuts

--- config.h
+++ config.h
@@ -29,8 +29,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	//{ "Gimp",     "gimp",       NULL,       1 << 5,            0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -68,6 +68,8 @@ static Key keys[] = {
   { MODKEY|ShiftMask,             XF86XK_Eject,      spawn,          SHCMD("poweroff") },
   { MODKEY,                       XF86XK_Eject,      spawn,          SHCMD("reboot") },
   { 0,                            XF86XK_Eject,      spawn,          SHCMD("light-locker-command -l") },
+	{ MODKEY,                       XK_q,              killclient,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,              quit,           {0} },
 
   //
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -81,7 +83,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -102,7 +103,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
